86b51c6109cd0fa1abeab540e326f3d76f185f31
Плохин <ploxind@bk.ru>
2024-09-12 10:07:15 +0400

    Создание проекта

---
+++ Practice/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.ssau.tk.nour.oop_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
